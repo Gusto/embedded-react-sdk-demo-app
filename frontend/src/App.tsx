87a232d4bed7455bb3eb1a1097181a81503480ce
Steve Jensen <steve.jensen@gusto.com>
2025-06-26 16:01:16 -0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,9 @@ import "./App.css";
 function App() {
   return (
     <GustoApiProvider config={{ baseUrl: "http://localhost:3001" }}>
+      {/* Place any SDK components here */}
       <EmployeeOnboardingFlow
-        companyId="{{companyId}}"
+        companyId="{{companyId}}" // Replace with your company ID
         onEvent={(eventType, eventPayload) => {
           console.log("eventType", eventType);
           console.log("eventPayload", eventPayload);
